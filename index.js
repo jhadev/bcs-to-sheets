1fa5f490dd9db928d395287244bd755ce6c09f11
JA <joshappeldev@gmail.com>
2019-06-24 01:33:21 -0400

    prompt has own file

--- index.js
+++ index.js
@@ -6,57 +6,18 @@ import { get } from 'http';
 import {
   email,
   password,
-  assignments,
   courseId,
   gradesEndpoint,
   loginEndpoint,
   spreadsheetId
 } from './utils/config';
+import prompt from './utils/prompt';
 import { authorize, getNewToken } from './utils/auth';
 import inquirer from 'inquirer';
 const tokenPath = './token.json';
 
 // query and sheet params homeworkTitle and selectionRange
 const params = {};
-
-const prompt = [
-  {
-    type: 'list',
-    name: 'doChoice',
-    message: 'What would you like to do?',
-    choices: [
-      'Get A Token From Google',
-      'Read from Google Sheets',
-      'Write To Google Sheets',
-      'Quit'
-    ]
-  },
-  {
-    type: 'input',
-    name: 'sheetChoice',
-    message: 'Enter your sheet name ex: Sheet1 or Week-1',
-    validate: input => (typeof input === 'string' ? true : false),
-    when: answer =>
-      answer.doChoice !== 'Quit' &&
-      answer.doChoice !== 'Get A Token From Google'
-  },
-  {
-    type: 'input',
-    name: 'selectionChoice',
-    message: 'Enter your sheet selection range in this format - A1:C',
-    validate: input => (typeof input === 'string' ? true : false),
-    when: answer =>
-      answer.doChoice !== 'Quit' &&
-      answer.doChoice !== 'Get A Token From Google'
-  },
-  {
-    type: 'list',
-    name: 'assignmentChoice',
-    message: 'Which assignment would you like to send to Google Sheets?',
-    choices: assignments,
-    when: answer => answer.doChoice === 'Write To Google Sheets'
-  }
-];
 
 const runPrompt = async () => {
   const {
